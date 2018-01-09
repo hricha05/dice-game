d5c5c372183b7dc66bf7f6f2e3c879a8a50fad3f
Harun <harunrich@googlemail.com>
2018-01-09 14:00:56 +0000

    improved game logic/ added dice image changes

--- app.js
+++ app.js
@@ -11,34 +11,58 @@
 // Initializations
 let score, roundScore, activePlayer;
 
-const player1Score = document.getElementById('player-0-score');
-const player2Score = document.getElementById('player-1-score');
+score = [0,0];
+roundScore = 0;
+activePlayer = 0;
 
-const current0 = document.getElementById('current-0');
-const current1 = document.getElementById('current-1');
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+// let player = document.querySelector('#current-' + activePlayer);
 
 
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. Roll the Dice
+   dice = Math.floor(Math.random() * 6 + 1);
 
-init();
+    //2. Display the result
+    const diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'assets/dice-' + dice + '.png';
 
-console.log(dice());
+    //3. Update thr round score IF the rolled number was not 1
+});
 
-function dice() {
-    const dice = Math.floor(Math.random() * 6 + 1);
-    return dice;
-    console.log(dice);
-}
+// init();
+// play();
+// newGame();
+// console.log(dice);
 
-function init() {
-    score = [0,0];
-    roundScore = 0;
-    activePlayer = 0;
+// function newGame() {
+//     document.getElementById('new-game').addEventListener("click", init);
+// }
 
-    // Reset player score card.
-    player1Score.textContent = 0;
-    player2Score.textContent = 0;
+// function play() {
+//     document.getElementById('diceRoll').addEventListener("click", function() {
+//         // update player score
+//         current1.textContent = dice;
+//         roundScore += dice;
+//         console.log(roundScore);
+//     });
+// }
+ 
+// function init() {
+//     score = [0,0];
+//     roundScore = 0;
+//     activePlayer = 0;
 
-    // Reset current score card.
-    current0.textContent = 0;
-    current1.textContent = 0;
-}
+//     // Reset player score card.
+//     player1Score.textContent = 0;
+//     player2Score.textContent = 0;
+
+//     // Reset current score card.
+//     current0.textContent = 0;
+//     current1.textContent = 0;
+// }
